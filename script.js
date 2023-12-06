555f847f1d6c879f3c4fea4314b83299ed7f9f0f
Thiago Meireles <thiago22md@gmail.com>
2023-12-06 13:54:03 -0300

    const_script

---
+++ script.js
@@ -0,0 +1,6 @@
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
