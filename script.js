d724f92cdcf60afc6ad4ab3901254f2cc4379f38
Thiago Meireles <thiago22md@gmail.com>
2023-12-06 13:56:26 -0300

    const_character

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
 const passIndicator = document.querySelector(".pass-indicator");
 const generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!$%&|[](){}:;.,*+-#@<>~"
+}
